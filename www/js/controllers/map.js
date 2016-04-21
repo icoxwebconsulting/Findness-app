24497ccacc21c842cf7b3ef5ffd4a961249e496b
davidjdr@icox.com <dafreiicox2015>
2016-04-20 20:07:07 -0430

    Agregado plugin de geolocalización móvil. En el mapa inicial se visualiza españa.

--- www/js/controllers/map.js
+++ www/js/controllers/map.js
@@ -6,8 +6,15 @@ app.controller('MapCtrl', function ($scope, $state, $ionicPlatform) {
         console.log("plataforma lista");
         var div = document.getElementById("map_canvas");
         if (div) {
+            const SPAIN = new plugin.google.maps.LatLng(39.9997938,-3.1926017);
+
             // Initialize the map view
-            map = plugin.google.maps.Map.getMap(div);
+            map = plugin.google.maps.Map.getMap(div, {
+                'camera': {
+                    'latLng': SPAIN,
+                    'zoom': 6
+                }
+            });
 
             // Wait until the map is ready status.
             // map.addEventListener(plugin.google.maps.event.MAP_READY, function(){
