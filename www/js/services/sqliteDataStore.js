518bc65ae2828b170ab9a8367b8bf9d3fc0aabc0
davidjdr@icox.com <dafreiicox2015>
2016-05-10 20:27:01 -0400

    Agregadas tablas de rutas

--- www/js/services/sqliteDataStore.js
+++ www/js/services/sqliteDataStore.js
@@ -1,108 +1,133 @@
 app.factory('sqliteDatastore', function ($q, APP_STORE_CONF) {
 
-        var db;
+    var db;
 
-        function getDbExist() {
-            return window.localStorage.getItem('db_exist') || null;
-        }
+    function getDbExist() {
+        return window.localStorage.getItem('db_exist') || null;
+    }
 
-        function setDbExist(dbExist) {
-            window.localStorage.setItem('db_exist', dbExist);
-        }
+    function setDbExist(dbExist) {
+        window.localStorage.setItem('db_exist', dbExist);
+    }
 
-        function openDatabase() {
-            var deferred = $q.defer();
+    function openDatabase() {
+        var deferred = $q.defer();
 
-            try {
-                if (!APP_STORE_CONF.webStore) {
-                    db = window.sqlitePlugin.openDatabase({name: "findness", location: 1}, function () {
-                        //success
-                        deferred.resolve(true);
-                    }, function (e) {
-                        //error
-                        deferred.reject(e);
-                    });
-                } else {
-                    db = window.openDatabase('findness', '1.0', 'findness database', 2 * 1024 * 1024);
+        try {
+            if (!APP_STORE_CONF.webStore) {
+                db = window.sqlitePlugin.openDatabase({name: "findness", location: 1}, function () {
+                    //success
                     deferred.resolve(true);
-                }
-            } catch (e) {
-                deferred.reject(e);
+                }, function (e) {
+                    //error
+                    deferred.reject(e);
+                });
+            } else {
+                db = window.openDatabase('findness', '1.0', 'findness database', 2 * 1024 * 1024);
+                deferred.resolve(true);
             }
-
-            return deferred.promise;
+        } catch (e) {
+            deferred.reject(e);
         }
 
-        function createTableTransactions() {
-            var query = 'CREATE TABLE IF NOT EXISTS transactions (' +
-                'id INTEGER PRIMARY KEY AUTOINCREMENT,' + //
-                'id_registered TEXT,' + //
-                'operator INTEGER NOT NULL,' + //
-                'reference TEXT,' + //
-                'status INT,' + //
-                'created DATETIME,' + //
-                'updated DATETIME)';
-            return execute(query);
-        }
+        return deferred.promise;
+    }
 
-        function createTableBalance() {
-            var query = 'CREATE TABLE IF NOT EXISTS balance (' +
-                'balance REAL NOT NULL,' +
-                'created DATETIME,' + //
-                'updated DATETIME)';
-            return execute(query);
-        }
+    function createTableTransactions() {
+        var query = 'CREATE TABLE IF NOT EXISTS transactions (' +
+            'id INTEGER PRIMARY KEY AUTOINCREMENT,' + //
+            'id_registered TEXT,' + //
+            'operator INTEGER NOT NULL,' + //
+            'reference TEXT,' + //
+            'status INT,' + //
+            'created DATETIME,' + //
+            'updated DATETIME)';
+        return execute(query);
+    }
 
-        function createTables() {
-            $q.all([
-                createTableTransactions(),
-                createTableBalance()
-            ]).then(function (value) {
-                setDbExist(true);
-            }, function (reason) {
-                // Error callback where reason is the value of the first rejected promise
+    function createTableBalance() {
+        var query = 'CREATE TABLE IF NOT EXISTS balance (' +
+            'balance REAL NOT NULL,' +
+            'created DATETIME,' + //
+            'updated DATETIME)';
+        return execute(query);
+    }
+
+    function createTableMapRoute() {
+        var query = 'CREATE TABLE IF NOT EXISTS map_route (' +
+            'id INTEGER PRIMARY KEY AUTOINCREMENT,' + //
+            'transport TEXT,' + //
+            'status INT,' + //
+            'created DATETIME,' + //
+            'updated DATETIME)';
+        return execute(query);
+    }
+
+    function createTableMapRoutePath() {
+        var query = 'CREATE TABLE IF NOT EXISTS map_route_path (' +
+            'id INTEGER PRIMARY KEY AUTOINCREMENT,' + //
+            'id_map_route INT,' +
+            'transport TEXT,' + //
+            'status INT,' + //
+            'start_point TEXT,' +
+            'end_point TEXT,' +
+            'created DATETIME,' + //
+            'updated DATETIME)';
+        return execute(query);
+    }
+
+    function createTables() {
+        $q.all([
+            createTableTransactions(),
+            createTableBalance(),
+            createTableMapRoute(),
+            createTableMapRoutePath()
+        ]).then(function (value) {
+            setDbExist(true);
+        }, function (reason) {
+            // Error callback where reason is the value of the first rejected promise
+        });
+    }
+
+    function execute(query, values) {
+        if (!values) {
+            values = [];
+        }
+        var deferred = $q.defer();
+
+        function exec() {
+            db.transaction(function (tx) {
+                tx.executeSql(query, values, function (tx, result) {
+                        deferred.resolve(result);
+                    },
+                    function (transaction, error) {
+                        deferred.reject(error);
+                    });
             });
         }
 
-        function execute(query, values) {
-            if (!values) {
-                values = [];
-            }
-            var deferred = $q.defer();
-
-            function exec() {
-                db.transaction(function (tx) {
-                    tx.executeSql(query, values, function (tx, result) {
-                            deferred.resolve(result);
-                        },
-                        function (transaction, error) {
-                            deferred.reject(error);
-                        });
+        if (!db) {
+            openDatabase()
+                .then(function () {
+                    exec();
                 });
-            }
-
-            if (!db) {
-                openDatabase()
-                    .then(function () {
-                        exec();
-                    });
-            } else {
-                exec();
-            }
-
-            return deferred.promise;
+        } else {
+            exec();
         }
 
-        function initDb() {
-            openDatabase();
-            if (!getDbExist()) {
-                createTables();
-            } else {
-            }
-        }
+        return deferred.promise;
+    }
 
-        return {
-            execute: execute,
-            initDb: initDb
-        };
-    });
+    function initDb() {
+        openDatabase();
+        if (!getDbExist()) {
+            createTables();
+        } else {
+        }
+    }
+
+    return {
+        execute: execute,
+        initDb: initDb
+    };
+});
