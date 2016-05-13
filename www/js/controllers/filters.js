2301f493f3142849015c827ac4127e9e8a109a4c
davidjdr@icox.com <dafreiicox2015>
2016-05-13 16:13:21 -0400

    Modificada la plantilla de los filtros CNAE.

--- www/js/controllers/filters.js
+++ www/js/controllers/filters.js
@@ -1,7 +1,5 @@
 app.controller('FiltersCtrl', function ($scope, $state, $filter, filterSrv) {
 
-    $scope.data = {};
-    $scope.data.pickupAfter = 5;
     $scope.model = "";
     $scope.clickedValueModel = "";
     $scope.removedValueModel = "";
@@ -11,8 +9,18 @@ app.controller('FiltersCtrl', function ($scope, $state, $filter, filterSrv) {
 
     });
 
+    function omitirAcentos(text) {
+        var acentos = "ÃÀÁÄÂÈÉËÊÌÍÏÎÒÓÖÔÙÚÜÛãàáäâèéëêìíïîòóöôùúüûÇç";
+        var original = "AAAAAEEEEIIIIOOOOUUUUaaaaaeeeeiiiioooouuuucc";
+        for (var i=0; i<acentos.length; i++) {
+            text = text.replace(acentos.charAt(i), original.charAt(i));
+        }
+        return text;
+    }
+
     $scope.getItems = function (query) {
         if (query && (query.length > 2 || (query[0] == '0' && query.length == 2) )) {
+            query = omitirAcentos(query);
             query = query.toLowerCase();
             return filterSrv.getCnaes(query).then(function (cnaes) {
                 return cnaes;
--- www/js/services/filters.js
+++ www/js/services/filters.js
@@ -3,13 +3,11 @@ app.factory('filterSrv', function ($q, $http, $filter, $timeout) {
     var cnaes;
 
     function readCnaesJson() {
-        console.log("leyendo json con cnaes");
         var deferred = $q.defer();
 
         $http.get('js/cnaes.json',
             {header: {'Content-Type': 'application/json; charset=UTF-8'}}
         ).then(function (cnaeArray) {
-            console.log(cnaeArray);
             deferred.resolve(cnaeArray.data);
         }).catch(function () {
             deferred.reject(false);
@@ -30,7 +28,6 @@ app.factory('filterSrv', function ($q, $http, $filter, $timeout) {
         if (!cnaes) {
             readCnaesJson().then(function (data) {
                 cnaes = data;
-                console.log("hizo el resolve ");
                 var filtered = filter(cnaes, query)
                 console.log("va a retornar", filtered.length, "de la búsqueda", query)
                 console.log(filtered);
@@ -39,7 +36,6 @@ app.factory('filterSrv', function ($q, $http, $filter, $timeout) {
                 });
             })
         } else {
-            console.log("ya existe cnae");
             var filtered = filter(cnaes, query)
             console.log("va a retornar", filtered.length, "de la búsqueda", query)
             console.log(filtered);
