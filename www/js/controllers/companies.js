51a3418a399a230b19d269c2e791238dfa91512c
Nestor Sanchez <nsanchez@smartbeemo.com>
2016-08-29 16:20:04 -0400

    Modified share list message

--- www/js/controllers/companies.js
+++ www/js/controllers/companies.js
@@ -36,7 +36,7 @@ app.controller('CompaniesCtrl', function ($rootScope, $scope, $state, $statePara
                     modalScope.modal.hide();
                 }).catch(function (error) {
                     $ionicPopup.alert({
-                        title: "No se pudo compartir la lista con el usuario"
+                        title: error.message
                     });
                 });
             }
