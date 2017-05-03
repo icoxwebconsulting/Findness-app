52b3bf750504f2418a2a7c908670a28bc1c3fdd1
nestor91sanchez <sanchez91nestor@gmail.com>
2017-05-03 19:57:10 -0400

    modified menu, list, window info

--- www/js/config.js
+++ www/js/config.js
@@ -1,4 +1,5 @@
 app.config(function ($ionicConfigProvider) {
     // Enable Native Scrolling on Android
     $ionicConfigProvider.platform.android.scrolling.jsScrolling(false);
+    $ionicConfigProvider.scrolling.jsScrolling(false);
 });
